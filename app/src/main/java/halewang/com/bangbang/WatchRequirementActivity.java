405a9366ef5d663bd77fea1b899dc04cdeadb859
halewang <862193071@qq.com>
2017-03-10 17:21:59 +0800

    加载更多和刷新

--- app/src/main/java/halewang/com/bangbang/WatchRequirementActivity.java
+++ app/src/main/java/halewang/com/bangbang/WatchRequirementActivity.java
@@ -3,6 +3,8 @@ package halewang.com.bangbang;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -12,7 +14,7 @@ import halewang.com.bangbang.view.WatchRequirementView;
 
 public class WatchRequirementActivity extends BaseActivity<WatchRequirementView, WatchRequirementPresenter> implements WatchRequirementView {
 
-    private ImageView ivBack;
+    private LinearLayout llBack;
     private LinearLayout llSearch;
     private TextView tvSelect;
     private SwipeRefreshLayout mRefreshLayout;
@@ -24,14 +26,24 @@ public class WatchRequirementActivity extends BaseActivity<WatchRequirementView,
         setContentView(R.layout.activity_watch_requirement);
         initView();
         mPresenter.onCreate();
+        initData();
     }
 
     private void initView() {
-        ivBack = (ImageView) findViewById(R.id.iv_back);
+        llBack = (LinearLayout) findViewById(R.id.ll_back);
         llSearch = (LinearLayout) findViewById(R.id.ll_search);
         tvSelect = (TextView) findViewById(R.id.tv_select);
         mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    }
+
+    private void initData(){
+        llBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/halewang/com/bangbang/presenter/WatchRequirementPresenter.java
+++ app/src/main/java/halewang/com/bangbang/presenter/WatchRequirementPresenter.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
@@ -28,7 +30,11 @@ public class WatchRequirementPresenter extends BasePresenter<WatchRequirementVie
     private Context mContext;
     private SwipeRefreshLayout mRefreshLayout;
     private RecyclerView mRecyclerView;
-    public WatchRequirementPresenter(Context mContext){
+    private RequirementAdapter mAdapter;
+    private int start = 0;
+    private final int LIMIT = 20;
+
+    public WatchRequirementPresenter(Context mContext) {
         this.mContext = mContext;
     }
 
@@ -37,28 +43,87 @@ public class WatchRequirementPresenter extends BasePresenter<WatchRequirementVie
         super.onCreate();
         initView();
         initData();
+        initRefresh();
     }
 
-    private void initView(){
+    private void initView() {
         mRefreshLayout = getMView().getRefreshLayout();
         mRecyclerView = getMView().getRecyclerView();
     }
 
-    private void initData(){
+    private void initData() {
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         BmobQuery<Requirement> query = new BmobQuery<>();
-        query.setLimit(40);
+        query.setLimit(LIMIT).order("-updatedAt");
+        start += LIMIT;
         query.findObjects(new FindListener<Requirement>() {
             @Override
             public void done(List<Requirement> list, BmobException e) {
-                if(e == null){
-                    Log.e(TAG, "done: "+list.toString() );
-                    mRecyclerView.setAdapter(new RequirementAdapter(list));
-                }else{
+                if (e == null) {
+                    Log.e(TAG, "done: " + list.toString());
+                    mAdapter = new RequirementAdapter(list);
+                    mAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+                        @Override
+                        public void onLoadMoreRequested() {
+                            loadMore();
+                        }
+                    });
+                    mRecyclerView.setAdapter(mAdapter);
+                } else {
                     Log.e(TAG, "查询失败: " + e.getMessage());
-                    Toast.makeText(mContext,"查询失败",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext, "查询失败", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+    }
+
+    private void loadMore() {
+        BmobQuery<Requirement> query = new BmobQuery<>();
+        query.setLimit(LIMIT)
+                .setSkip(start)
+                .order("-updatedAt")
+                .findObjects(new FindListener<Requirement>() {
+                    @Override
+                    public void done(List<Requirement> list, BmobException e) {
+                        if (e == null) {
+                            if (list.size() < LIMIT) {
+                                mAdapter.loadMoreEnd();
+                            }
+                            mAdapter.addData(list);
+                            mAdapter.notifyDataSetChanged();
+                        } else {
+                            Log.e(TAG, "查询失败: " + e.getMessage());
+                            Toast.makeText(mContext, "加载失败", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        start += LIMIT;
+    }
+
+    private void initRefresh(){
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                BmobQuery<Requirement> query = new BmobQuery<>();
+                query.setLimit(LIMIT).order("-updatedAt");
+                start = LIMIT;
+                query.findObjects(new FindListener<Requirement>() {
+                    @Override
+                    public void done(List<Requirement> list, BmobException e) {
+                        if (e == null) {
+                            mAdapter.refreshData(list);
+                        } else {
+                            Toast.makeText(mContext, "查询失败", Toast.LENGTH_SHORT).show();
+                        }
+                        mRefreshLayout.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRefreshLayout.setRefreshing(false);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
